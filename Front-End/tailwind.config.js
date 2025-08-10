011448598d4bd7e44348c4541b6ed7c848a3a174
Micharrieta <micharrieta10@gmail.com>
2025-08-09 23:11:01 -0500

    Dependencies updated and Tailwind CLI added

---
+++ Front-End/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        "./Front-End/**/*.html",
+        "./Front-End/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
